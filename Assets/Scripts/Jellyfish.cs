68c356afe84d99c9a5cb04b6d26a1c191285fc9e
alialamin662 <32805016+alialamin662@users.noreply.github.com>
2017-10-14 22:39:50 -0400

    dkal'djf

---
+++ Assets/Scripts/Jellyfish.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jellyfish : MonoBehaviour {
+    public float speed;
+    public float radius;
+    public float power;
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        this.transform.RotateAround(this.transform.up, 180);
+
+        GameObject explosionInstance = Instantiate(explosion);
+        explosionInstance.transform.position = this.transform.position;
+        Destroy(this.gameObject);
+    }
+
+   
+      
+    }

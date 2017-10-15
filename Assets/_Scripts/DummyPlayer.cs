b8e908869a915a4d98acdef190a3d784ad4ec135
Ben <coobenja@umich.edu>
2017-10-14 20:41:21 -0400

    Ben Initial Push

---
+++ Assets/_Scripts/DummyPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayer : MonoBehaviour {
+
+    float x;
+    float y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(-x, y, 0));
+    }
+}

853742bcfeafdda6c54900c364d4f5e51f84dbc0
Ben <coobenja@umich.edu>
2017-10-14 21:56:24 -0400

    Boss Laser Works Now!

---
+++ Assets/Scripts/EyeDetector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeDetector : MonoBehaviour {
+
+    public int target = 0;
+    public GameObject targetGO;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetGO != null) {
+
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.tag == "Player") && (targetGO == null))
+        {
+            //target = other.gameObject.GetComponent<flightController>().playerNumber;
+            targetGO = other.gameObject;
+            //other.gameObject.GetComponent<flightController>().numMissilesFollowing++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((other.gameObject.tag == "Player") && (targetGO == other.gameObject))
+        {
+            //target = 0;
+            targetGO = null;
+            //other.gameObject.GetComponent<flightController>().numMissilesFollowing--;
+
+        }
+    }
+}
--- Assets/_Scripts/EyeLaser.cs
+++ Assets/_Scripts/EyeLaser.cs
@@ -8,31 +8,55 @@ public class EyeLaser : MonoBehaviour {
     public GameObject firePoint;
     public GameObject laser;
     public float laserSpeed = 10f;
+    public float fireRate = 1f;
+    float fireInterval = 0f;
+
+    public float health = 20f;
+
+    public EyeDetector eyeDetector;
 
 	// Use this for initialization
 	void Start () {
 
         firePoint = transform.Find("FirePoint").gameObject;
 
+        eyeDetector = GameObject.Find("EyeDetectionRange").GetComponent<EyeDetector>();
+
         //Placeholder Target Selection Code: REPLACE THIS
-        targetPlayer = GameObject.Find("Dummy_Player");
+        //targetPlayer = GameObject.Find("Dummy_Player");
 	}
 
     void Update() {
-        FireLaser();
+        if (targetPlayer != null)
+        {
+            transform.LookAt(targetPlayer.transform.position);
+            FireLaser();
+        }
+        targetPlayer = eyeDetector.targetGO;
     }
 
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.LookAt(targetPlayer.transform.position);
+        
 
-	}
+    }
 
     void FireLaser() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Time.timeSinceLevelLoad > fireInterval) {
             GameObject laserclone;
             laserclone = Instantiate(laser, firePoint.transform.position, firePoint.transform.rotation) as GameObject;
             laserclone.GetComponent<Rigidbody>().velocity = laserclone.transform.forward * laserSpeed;
+            fireInterval = Time.timeSinceLevelLoad + fireRate;
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Bullet") {
+            health -= 1;
+
+            if (health <= 0) {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
